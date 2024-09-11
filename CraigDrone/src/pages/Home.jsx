1d206d6a5ba60bd3a6be1894c8a47159b5966f00
Luis Magana <49489590+MaganaLu@users.noreply.github.com>
2024-09-10 21:40:14 -0700

    small fixes

--- CraigDrone/src/pages/Home.jsx
+++ CraigDrone/src/pages/Home.jsx
@@ -8,23 +8,30 @@ import FlowChart from '../assets/FlowChart.svg';
 import HomeCarousel from '../components/HomeCarousel';
 import ContactForm from '../components/ContactForm';
 
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
 
 import './Home.css';
 
 
 const Home = () => {
+
+    const navigate = useNavigate();
+    const handleGoToContact = () => navigate("/Contact");
+
     return (
         <>
 
             <div className="videoContainer">
-            <video id='videoPlayer' autoPlay loop muted >
+                <video id='videoPlayer' autoPlay loop muted >
                     <source src={video} type="video/mp4" />
                 </video>
-                
+
                 <div className="overlayText">
                     <p id="topText">Premium Full Spectrum Aerial Photography and Videography Solutions </p>
                     <p id="topSubText">For your personal or business needs in WA.</p>
-                    <button className='CustomButton' onClick={() => alert("You clicked the button")}>Contact Us!</button>
+                    <button className='CustomButton' onClick={handleGoToContact}>Contact Us!</button>
                 </div>
             </div>
             <img className="wavesSVG" src={colorWaves} />
@@ -46,37 +53,37 @@ const Home = () => {
                 </p>
             </div>
 
-            <HomeCarousel/>
-            
+            <HomeCarousel />
+
             <div className='flowchartContainer'>
-                <img className="flowchartImage" src={FlowChart} />  
+                <img className="flowchartImage" src={FlowChart} />
             </div>
 
-            <button className='CustomButton' style={{marginTop:"50px"}} onClick={() => alert("You clicked the button")}>Book Now!</button>
+            <button className='CustomButton' style={{ marginTop: "50px" }} onClick={handleGoToContact}>Book Now!</button>
 
             <div className='OneStopContainer'>
-            <h1 style={{paddingTop:'50px'}}>Your One-Stop-Shop for Drone Services anywhere in Seattle!</h1>
-                <p>When searching for “high-quality drone video services near me” you want to ensure you select the experts. "CraigDrones"  provides premium aerial photography, videography, 
-                    and industrial drone services at an affordable price. 
-                </p> 
+                <h1 style={{ paddingTop: '50px' }}>Your One-Stop-Shop for Drone Services anywhere in Seattle!</h1>
+                <p>When searching for “high-quality drone video services near me” you want to ensure you select the experts. "CraigDrones"  provides premium aerial photography, videography,
+                    and industrial drone services at an affordable price.
+                </p>
 
                 <p>
-                    We also specialize in the production of that video into vibrant media assets, sure to engage your audience. 
-                    We have experience in Fife, Puyallup, Downtown Seattle, Capitol Hill, Wallingford & Greenlake, North Queen Anne, West Seattle, Beacon Hill, and the surrounding areas. 
-                </p> 
-                <p> 
-                    From drones to so much more, "CraigDrones" is your one-stop-shop for capturing the best imagery of your business, property, or event from air, land, and sea. 
+                    We also specialize in the production of that video into vibrant media assets, sure to engage your audience.
+                    We have experience in Fife, Puyallup, Downtown Seattle, Capitol Hill, Wallingford & Greenlake, North Queen Anne, West Seattle, Beacon Hill, and the surrounding areas.
+                </p>
+                <p>
+                    From drones to so much more, "CraigDrones" is your one-stop-shop for capturing the best imagery of your business, property, or event from air, land, and sea.
                     Elevate your work with our drone photography and videography services. Give us a call to get an instant quote and timeline.
-                </p> 
+                </p>
 
             </div>
 
             <div id="ContactSection">
-                <ContactForm/>
+                <ContactForm />
             </div>
 
 
-            
+
         </>
     )
 };
--- CraigDrone/src/pages/Services.jsx
+++ CraigDrone/src/pages/Services.jsx
@@ -4,13 +4,18 @@ import './Services.css'
 import PageHeader from '../components/PageHeader';
 import ServicesCardComponent from '../components/ServicesCardComponent';
 
+import { useNavigate } from "react-router-dom";
+
 const Services = () => {
+  const navigate = useNavigate();
+  const handleGoToContact = () => navigate("/Contact");
+
   return (
     <>
       <PageHeader hText="Services" />
       <div className='ServicesContainer'>
         <h1>The Drone Services you Need, Whatever the Project!</h1>
-        <button>Book Us Now!</button>
+        <button onClick={handleGoToContact}>Book Us Now!</button>
         <p>You can get exactly the aerial media you need regardless of your project requirements. That’s because at Sky Solutions NW we offer a variety of high-quality drone services.</p>
         <p>We are experts in the taking and editing of both photography and videography. Here’s a list of our most popular services.</p>
         <p> Contact us if you need something not listed here!</p>
