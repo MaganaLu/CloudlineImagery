2edddef4b60fcd2b644d4e16d72c3250ad7f845d
Luis Magana <49489590+MaganaLu@users.noreply.github.com>
2024-10-01 18:31:38 -0700

    new portfolio sections

--- CraigDrone/src/pages/Portfolio.jsx
+++ CraigDrone/src/pages/Portfolio.jsx
@@ -78,7 +78,35 @@ const Portfolio = () => {
           </ul>
         )}
       </div>
+
       <hr id='#Real-Estate' className="hr-text" data-content="Real Estate"/>
+      <div className="portfolioCardContainer">
+        {realEstateEntries.map(portfolioEntry =>
+          <ul key={portfolioEntry.title} className="ulPortfolio">
+            <PortfolioCardComponent description={portfolioEntry.description} video={portfolioEntry.video_url} title={portfolioEntry.title} date={portfolioEntry.date} image={portfolioEntry.image} type={portfolioEntry.type} />
+          </ul>
+        )}
+      </div>
+
+      <hr id='#Construction' className="hr-text" data-content="Construction"/>
+      <div className="portfolioCardContainer">
+        {realEstateEntries.map(portfolioEntry =>
+          <ul key={portfolioEntry.title} className="ulPortfolio">
+            <PortfolioCardComponent description={portfolioEntry.description} video={portfolioEntry.video_url} title={portfolioEntry.title} date={portfolioEntry.date} image={portfolioEntry.image} type={portfolioEntry.type} />
+          </ul>
+        )}
+      </div>
+
+      <hr id='#Event-Coverage' className="hr-text" data-content="Event Coverage"/>
+      <div className="portfolioCardContainer">
+        {realEstateEntries.map(portfolioEntry =>
+          <ul key={portfolioEntry.title} className="ulPortfolio">
+            <PortfolioCardComponent description={portfolioEntry.description} video={portfolioEntry.video_url} title={portfolioEntry.title} date={portfolioEntry.date} image={portfolioEntry.image} type={portfolioEntry.type} />
+          </ul>
+        )}
+      </div>
+
+      <hr id='#Other' className="hr-text" data-content="Other Services"/>
       <div className="portfolioCardContainer">
         {realEstateEntries.map(portfolioEntry =>
           <ul key={portfolioEntry.title} className="ulPortfolio">
